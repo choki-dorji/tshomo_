898091e963bfdbb1059b45cd3df12c12aeb1e327
choki-dorji <62417033+choki-dorji@users.noreply.github.com>
2022-01-06 11:39:08 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3gjm^_scxasa&ewe64tb$ass7t)z*a45aycsqnwo3j!+^=xq&j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'bumthap123.herokuapp.com']
 
 
 # Application definition
